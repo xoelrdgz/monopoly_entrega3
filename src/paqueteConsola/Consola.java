d2472a96b4d7e87528d10394c6935758305906c1
Xoel Rodriguez <xoel2r@gmail.com>
2024-11-13 13:49:59 +0100

    Consola

--- src/paqueteConsola/Consola.java
+++ src/paqueteConsola/Consola.java
@@ -1,5 +1,9 @@
 package paqueteConsola;
 
 public interface Consola {
-    
+
+    public void imprimir (String mensaje);
+
+    public String leer (String mensaje);
+
 }
--- src/paqueteConsola/ConsolaNormal.java
+++ src/paqueteConsola/ConsolaNormal.java
@@ -1,5 +1,23 @@
 package paqueteConsola;
 
+import java.util.Scanner;
+
 public class ConsolaNormal implements Consola {
-    
+
+    @Override
+    public void imprimir (String mensaje){
+        System.out.println(mensaje);
+    }
+
+    @Override
+    public String leer (String mensaje){
+        Scanner scanner = new Scanner(System.in);
+
+        mensaje = scanner.nextLine();
+
+        scanner.close();
+
+        return mensaje;
+    }
+
 }
