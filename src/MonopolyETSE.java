59a1231c4d85df9832d29a2882227c3b5247039a
diegofernandezmarquez <DKDDKD@GMAIL.COM>
2024-11-13 13:57:32 +0100

    consola

--- src/MonopolyETSE.java
+++ src/MonopolyETSE.java
@@ -1,2 +1,10 @@
+
 public class MonopolyETSE {
+
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+
+        // Llamar al metodo para iniciar la partida
+        Juego.iniciarPartida();
+    }
 }
--- src/paqueteJuego/Juego.java
+++ src/paqueteJuego/Juego.java
@@ -1,5 +1,9 @@
 package paqueteJuego;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Juego {
-    
+
+
 }
